10a7d9fee10791856d97f3ddfbd8025094e00952
Aurélien Deharbe <aurelien.deharbe@gmail.com>
2022-09-20 12:01:44 +0200

    fix count bug in checkupdates for waybar

--- .config/waybar/scripts/pacman_updates.sh
+++ .config/waybar/scripts/pacman_updates.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 updates="$(checkupdates)"
-num_updates="$(echo -n "$updates" | wc -l)"
-if [[ $num_updates == 0 ]]; then
+if [[ $? == 0 ]]; then
+    num_updates="$(echo "$updates" | wc -l)"
+    class="pending"
+    percentage=0
+else
     num_updates=""
     class="uptodate"
     percentage=0
-else
-    num_updates="$(echo "$updates" | wc -l)"
-    class="pending"
-    percentage=100
 fi
 
 tooltip=$(echo "$updates" | sed -z 's/\n/\\n/g')
