3f9fac2c38877ccc04560eddd40010f99d82fd63
aurelien.deharbe@gmail.com <Aurelien Deharbe>
2022-11-07 16:05:00 +0100

    nvim initial config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,71 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd('packadd packer.nvim')
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- general settings
+  vim.keymap.set('i', 'kj', '<Esc>')
+  vim.g.mapleader = ','
+  vim.g.encoding = 'utf-8'
+
+  -- indentations
+  vim.opt.expandtab = true
+  vim.opt.shiftwidth = 2
+
+  -- line numbering: relative when focused, absolute otherwise
+  vim.cmd('set signcolumn=yes')
+  vim.cmd('set number relativenumber')
+  local augroup = vim.api.nvim_create_augroup('numbertoggle', { clear = true })
+  vim.api.nvim_create_autocmd(
+    { 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter' },
+    { group = augroup, pattern = '*', command = 'if &nu && mode() != "i" | set rnu | endif' }
+  )
+  vim.api.nvim_create_autocmd(
+    { 'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave' },
+    { group = augroup, pattern = '*', command = 'if &nu | set nornu | endif' }
+  )
+
+  -- colors
+  use 'edeneast/nightfox.nvim'
+  vim.cmd('colorscheme nightfox')
+
+  -- status bar
+  use 'nvim-lualine/lualine.nvim'
+  require('lualine').setup {
+    options = {
+      theme = 'nightfox'
+    }
+  }
+
+  -- tabs and buffers
+  use 'nanozuki/tabby.nvim'
+  require('tabby').setup()
+
+  -- file tree
+  use {
+    'nvim-neo-tree/neo-tree.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'muniftanjim/nui.nvim',
+      'kyazdani42/nvim-web-devicons',
+    }
+  }
+  require('neo-tree').setup()
+
+  -- end of config, bootstraping packer
+  if packer_bootsrap then
+    require('packer').sync()
+  end
+end)
+
